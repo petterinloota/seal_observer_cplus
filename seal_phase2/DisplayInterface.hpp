3d948c37c4cd4951d707494313866c6faf542e3a
Pet Teri <petterinloota@gmail.com>
2020-04-11 19:32:15 +0300

    Removed phase2 ... as void

--- seal_phase2/DisplayInterface.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <string>
-
-class DisplayInterface
-{
-public:
-    virtual void Update(int nr); // =0 is necessary if a pure virtual function ... no local implementation   
-};
-
